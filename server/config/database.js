8d855d99b7b7d0d3c9014b5f3427af3bc80297ef
Balkrishna09 <balkrishna4000021@gmail.com>
2025-04-21 09:20:08 +0300

    Revert "Local DB supported"
    This reverts commit 53a26c725b97bed8adbc57801246c7ef5dc3d946.

--- server/config/database.js
+++ server/config/database.js
@@ -5,8 +5,11 @@ const { MONGODB_URL } = process.env;
 
 exports.connect = () => {
 	mongoose
-		.connect(MONGODB_URL)
-		.then(() => console.log(`DB Connection Success`))
+		.connect(MONGODB_URL, {
+			useNewUrlparser: true,
+			useUnifiedTopology: true,
+		})
+		.then(console.log(`DB Connection Success`))
 		.catch((err) => {
 			console.log(`DB Connection Failed`);
 			console.log(err);
--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+		origin: "*",
 		credentials: true,
 	})
 );
